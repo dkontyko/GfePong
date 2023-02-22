3c059a342ea77de8c93eccbcfab9c8923cbd9a98
David Kontyko <33853225+dkontyko@users.noreply.github.com>
2023-02-22 17:29:07 -0500

    adding graphics and events

--- Pong.cs
+++ Pong.cs
@@ -10,19 +10,59 @@ namespace GfePong {
                 StartPosition = FormStartPosition.CenterScreen
             };
 
-
             mainWindow.ShowDialog();
         }
     }
 
     partial class MainForm : Form {
 
+        private readonly Brush blackBrush = new SolidBrush(Color.Black);
+        private Rectangle leftBumper = new Rectangle(5, 5, 10, 50);
+
+        private int leftMovement = 0;
+
+        private readonly Timer leftBumperMovementTimer = new Timer();
+
+        public MainForm() : base() {
+            leftBumperMovementTimer.Enabled = true;
+            leftBumperMovementTimer.Interval = 50;
+            leftBumperMovementTimer.Tick += LeftBumperMovementTimer_Tick;
+
+            this.SetStyle(ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+
+        }
+
+        private void LeftBumperMovementTimer_Tick(object sender, System.EventArgs e) {
+            leftBumper.Y += leftMovement;
+
+            this.Invalidate();
+        }
+
         protected override void OnPaint(PaintEventArgs e) {
             base.OnPaint(e);
-
-            var blackBrush = new SolidBrush(Color.Black);
-            var leftBumper = new Rectangle(5, 5, 10, 50);
             e.Graphics.FillRectangle(blackBrush, leftBumper);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e) {
+            base.OnKeyDown(e);
+
+            if (e.KeyCode == Keys.Down) {
+                leftMovement = 5;
+            } else if (e.KeyCode == Keys.Up) {
+                leftMovement = -5;
+            }
+
+            this.Invalidate();
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e) {
+            base.OnKeyUp(e);
+
+            if(e.KeyCode == Keys.Down || e.KeyCode == Keys.Up) {
+                leftMovement = 0;
+            }
+
+            this.Invalidate();
         }
     }
 }
